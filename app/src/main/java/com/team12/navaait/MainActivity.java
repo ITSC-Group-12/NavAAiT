9dffddb7c5390c943930f0250eb1fda75ea2e0d7
JaCkIsO <samteblak@gmail.com>
2017-05-06 16:11:34 +0300

    Add FloatingSearchView - Add Location Callout - Add Location Display

--- app/src/main/java/com/team12/navaait/MainActivity.java
+++ app/src/main/java/com/team12/navaait/MainActivity.java
@@ -2,6 +2,7 @@ package com.team12.navaait;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
@@ -12,17 +13,23 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.MobileMapPackage;
+import com.esri.arcgisruntime.mapping.view.Callout;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 import java.io.File;
@@ -39,12 +46,20 @@ public class MainActivity extends AppCompatActivity
     private static String mmpkFilePath;
     private MapView mMapView;
     private MobileMapPackage mapPackage;
+    private FloatingSearchView mSearchView;
+
+    private static final String sTag = "Gesture";
+    private Callout mCallout;
+
+    private LocationDisplay mLocationDisplay;
 
     // define permission to request
-    String[] reqPermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    private int requestCode = 2;
+    String[] reqPermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+            .ACCESS_COARSE_LOCATION};
+    private static final int requestCode1 = 2;
+    private static final int requestCode2 = 3;
 
-    private static String createMobileMapPackageFilePath(){
+    private static String createMobileMapPackageFilePath() {
         return extStorDir.getAbsolutePath() + File.separator + extSDCardDirName + File.separator + filename + FILE_EXTENSION;
     }
 
@@ -52,8 +67,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -63,15 +76,13 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+        mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        mSearchView.attachNavigationDrawerToMenuButton(drawer);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         // get sdcard resource name
         extStorDir = Environment.getExternalStorageDirectory();
         // get the directory
@@ -84,27 +95,130 @@ public class MainActivity extends AppCompatActivity
         // retrieve the MapView from layout
         mMapView = (MapView) findViewById(R.id.mapView);
 
+        // get the MapView's LocationDisplay
+        mLocationDisplay = mMapView.getLocationDisplay();
+
         // For API level 23+ request permission at runtime
-        if(ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[0]) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[0]) == PackageManager.PERMISSION_GRANTED) {
             loadMobileMapPackage(mmpkFilePath);
-        }else{
+        } else {
             // request permission
-            ActivityCompat.requestPermissions(MainActivity.this, reqPermission, requestCode);
+            ActivityCompat.requestPermissions(MainActivity.this, reqPermission, requestCode1);
+        }
+
+
+        mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+                Log.d(sTag, "onSingleTapConfirmed: " + motionEvent.toString());
+
+                // get the point that was clicked and convert it to a point in map coordinates
+                android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()),
+                        Math.round(motionEvent.getY()));
+                // create a map point from screen point
+                Point mapPoint = mMapView.screenToLocation(screenPoint);
+                // convert to WGS84 for lat/lon format
+                Point wgs84Point = (Point) GeometryEngine.project(mapPoint, SpatialReferences.getWgs84());
+                // create a textview for the callout
+                TextView calloutContent = new TextView(getApplicationContext());
+                calloutContent.setTextColor(Color.BLACK);
+                calloutContent.setSingleLine();
+                // format coordinates to 4 decimal places
+                calloutContent.setText("Lat: " + String.format("%.4f", wgs84Point.getY()) +
+                        ", Lon: " + String.format("%.4f", wgs84Point.getX()));
+
+                // get callout, set content and show
+                mCallout = mMapView.getCallout();
+                mCallout.setLocation(mapPoint);
+                mCallout.setContent(calloutContent);
+                mCallout.show();
+
+                // center on tapped point
+                mMapView.setViewpointCenterAsync(mapPoint);
+
+                return true;
+            }
+        });
+
+        // Listen to changes in the status of the location data source.
+        mLocationDisplay.addDataSourceStatusChangedListener(new LocationDisplay.DataSourceStatusChangedListener() {
+            @Override
+            public void onStatusChanged(LocationDisplay.DataSourceStatusChangedEvent dataSourceStatusChangedEvent) {
+
+                // If LocationDisplay started OK, then continue.
+                if (dataSourceStatusChangedEvent.isStarted())
+                    return;
+
+                // No error is reported, then continue.
+                if (dataSourceStatusChangedEvent.getError() == null)
+                    return;
+
+                // If an error is found, handle the failure to start.
+                // Check permissions to see if failure may be due to lack of permissions.
+                boolean permissionCheck1 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[1]) ==
+                        PackageManager.PERMISSION_GRANTED;
+                boolean permissionCheck2 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[2]) ==
+                        PackageManager.PERMISSION_GRANTED;
+
+                if (!(permissionCheck1 && permissionCheck2)) {
+                    // If permissions are not already granted, request permission from the user.
+                    ActivityCompat.requestPermissions(MainActivity.this, reqPermission, requestCode2);
+                } else {
+                    // Report other unknown failure types to the user - for example, location services may not
+                    // be enabled on the device.
+                    String message = String.format("Error in DataSourceStatusChangedListener: %s", dataSourceStatusChangedEvent
+                            .getSource().getLocationDataSource().getError().getMessage());
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+        // start showing location
+        if (!mLocationDisplay.isStarted()) {
+            mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+            mLocationDisplay.startAsync();
         }
 
     }
 
     /**
      * Handle the permissions request response
-     *
      */
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            loadMobileMapPackage(mmpkFilePath);
-        }else{
-            // report to user that permission was denied
-            Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied),
-                    Toast.LENGTH_SHORT).show();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+
+            case requestCode1: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadMobileMapPackage(mmpkFilePath);
+                } else {
+                    // report to user that permission was denied
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            case requestCode2: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location permission was granted. This would have been triggered in response to failing to start the
+                    // LocationDisplay, so try starting this again.
+                    mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+                    mLocationDisplay.startAsync();
+                } else {
+                    // If permission was denied, show toast to inform user what was chosen. If LocationDisplay is started again,
+                    // request permission UX will be shown again, option should be shown to allow never showing the UX again.
+                    // Alternative would be to disable functionality so request is not shown again.
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied), Toast
+                            .LENGTH_SHORT).show();
+                }
+            }
+
+
         }
     }
 
@@ -113,7 +227,7 @@ public class MainActivity extends AppCompatActivity
      *
      * @param mmpkFile Full path to mmpk file
      */
-    private void loadMobileMapPackage(String mmpkFile){
+    private void loadMobileMapPackage(String mmpkFile) {
         //[DocRef: Name=Open Mobile Map Package-android, Category=Work with maps, Topic=Create an offline map]
         // create the mobile map package
         mapPackage = new MobileMapPackage(mmpkFile);
@@ -125,10 +239,11 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void run() {
                 // check load status and that the mobile map package has maps
-                if(mapPackage.getLoadStatus() == LoadStatus.LOADED && mapPackage.getMaps().size() > 0){
+                if (mapPackage.getLoadStatus() == LoadStatus.LOADED && mapPackage.getMaps().size() > 0) {
                     // add the map from the mobile map package to the MapView
                     mMapView.setMap(mapPackage.getMaps().get(0));
-                }else{
+
+                } else {
                     // Log an issue if the mobile map package fails to load
                     Log.e(TAG, mapPackage.getLoadError().getMessage());
                 }
@@ -145,28 +260,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -193,4 +286,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
